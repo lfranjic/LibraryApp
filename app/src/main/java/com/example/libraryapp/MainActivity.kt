15b42d6acceee73615b29b4484ee647ccd1e57a4
lfranjic <leofranjic46@gmail.com>
2025-06-11 11:49:41 +0200

    Committing entire app

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
